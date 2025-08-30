06a933b19dddd9f479a2bd05ce399232282f0efe
Mateus-Sebastian <mateussebastian1@gmail.com>
2025-08-30 15:05:56 -0300

    oitava questão da atividade - dicionários e tuplas

---
+++ Aula 7/atividades_dicionarios_e_tuplas/questao8.py
@@ -0,0 +1,19 @@
+pontos_turisticos = {
+    (-8.1584, -34.9224): 'Cristo Redentor',
+    (-12.9714, -38.5014): 'Elevador Lacerda',
+    (-7.2034, -39.3206): 'Açude Velho'
+}
+
+coordenadas = (input("Digite as coordenadas (latitude, longitude): ").split(','))
+
+for i in range(len(coordenadas)):
+    coordenadas[i] = float(coordenadas[i])
+
+print(coordenadas)
+
+for i in pontos_turisticos.keys():
+    if coordenadas == list(i):
+        print(f'Ponto turístico: {pontos_turisticos[i]}')
+        break
+else:
+    print('Coordenadas não encontradas.')
